c808f2470ec4ffe42ffda56dabae5823e65cd064
Sunny Kumar <sunnyshah12153@gmail.com>
2026-02-27 20:57:09 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Creating directory
+mkdir -p src
+mkdir -p research
+
+
+#Creating files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created succesfully!."
